c7cdefec161197b1b5cc385be71042b5201cafd4
benjamin <benjamin@le-filament.com>
2022-08-25 16:44:36 +0200

    [clean] delete unused image

--- controllers/main.py
+++ controllers/main.py
@@ -3,8 +3,7 @@
 
 import json
 
-from odoo import http
-from odoo import exceptions
+from odoo import exceptions, http
 from odoo.http import request
 
 
@@ -13,19 +12,27 @@ class ReleaseAgent(http.Controller):
     # Routes
     # ------------------------------------------------------
     @http.route(
-        ["/get-modules"], type="http", auth="none", method=["GET"], csrf=False,
+        ["/get-modules"],
+        type="http",
+        auth="none",
+        method=["GET"],
+        csrf=False,
     )
     def get_modules(self):
         db_header = request.httprequest.headers.get("DbName", False)
         if db_header and db_header == request.db:
-            modules = request.env["ir.module.module"].sudo().search([
-                ("state", "=", "installed")
-            ])
-            res = modules.mapped(lambda m: {
-                "name": m.name,
-                "author": m.author,
-                "author_type": self.get_author_type(m),
-            })
+            modules = (
+                request.env["ir.module.module"]
+                .sudo()
+                .search([("state", "=", "installed")])
+            )
+            res = modules.mapped(
+                lambda m: {
+                    "name": m.name,
+                    "author": m.author,
+                    "author_type": self.get_author_type(m),
+                }
+            )
             return http.Response(
                 response=json.dumps(res),
                 headers=[("Content-Type", "application/json")],
