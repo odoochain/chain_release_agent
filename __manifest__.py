51feadc2e34ac21ce0c7d69ad626506810968122
Rémi - Le Filament <remi@le-filament.com>
2022-12-20 20:41:18 +0100

    [MIG][16.0]

--- __manifest__.py
+++ __manifest__.py
@@ -3,10 +3,10 @@
     "summary": "Renvoi les modules installés",
     "author": "Le Filament",
     "website": "https://le-filament.com",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
-        "base",
+        "base_setup",
     ],
     "data": ["views/base_config_view.xml"],
     "installable": True,
